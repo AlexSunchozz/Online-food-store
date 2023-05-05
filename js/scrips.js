f696e9fdc17378739a6a8a25ed053cca797285da
AlexandrRepkin <arepkin96@mail.ru>
2023-05-05 16:00:53 +0300

    Modal

--- js/scrips.js
+++ js/scrips.js
@@ -100,4 +100,52 @@ window.addEventListener('DOMContentLoaded', function() {
     }   
 
     setClock('.timer', deadline)
+
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show')
+        document.body.style.overflow = ''
+    }
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'
+        clearInterval(modalTimerId)
+    }
+
+    const modalTimerId = setTimeout(openModal, 5000)
+
+    modalTrigger.forEach(e => {
+        e.addEventListener('click', openModal)
+    })
+
+    modalCloseBtn.addEventListener('click', closeModal)
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal()
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) closeModal()
+    })
+
+    function shpwModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+            openModal()
+            window.removeEventListener('scroll', shpwModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', shpwModalByScroll)
 });
