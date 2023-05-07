f009e7fa475a0fab0827d44d2e105987e9442c83
AlexandrRepkin <arepkin96@mail.ru>
2023-05-07 17:07:58 +0300

    Server Forms

--- js/scrips.js
+++ js/scrips.js
@@ -105,10 +105,8 @@ window.addEventListener('DOMContentLoaded', function() {
     // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close]');
+          modal = document.querySelector('.modal');
     
-
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show')
@@ -122,16 +120,14 @@ window.addEventListener('DOMContentLoaded', function() {
         clearInterval(modalTimerId)
     }
 
-    const modalTimerId = setTimeout(openModal, 5000)
+    const modalTimerId = setTimeout(openModal, 50000)
 
     modalTrigger.forEach(e => {
         e.addEventListener('click', openModal)
     })
 
-    modalCloseBtn.addEventListener('click', closeModal)
-
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') === '') {
             closeModal()
         }
     })
@@ -220,4 +216,123 @@ window.addEventListener('DOMContentLoaded', function() {
         '.menu .container',
         'menu__item'
     ).render();
+
+
+    // Server Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+    
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    // 1) Формат строчный
+
+    
+    // function postData(form) {
+    //     form.addEventListener('submit', (e) => {
+    //         e.preventDefault();
+
+    //         const statusMessage = document.createElement('div');
+    //         statusMessage.classList.add('status');
+    //         statusMessage.textContent = message.loading;
+
+    //         form.append(statusMessage);
+
+    //         const request = new XMLHttpRequest();
+    //         request.open('POST', 'server.php');
+
+    //         const formData = new FormData(form);
+
+    //         request.send(formData);
+
+    //         request.addEventListener('load', () => {
+    //             if (request.status === 200) {
+    //                 console.log(request.response);
+    //                 statusMessage.textContent = message.success;
+    //                 form.reset();
+    //                 setTimeout(() => {
+    //                     statusMessage.remove()
+    //                 }, 2000)
+    //             } else {
+    //                 statusMessage.textContent = message.failure;
+    //             }
+    //         })
+    //     })
+    // }
+
+
+    // 2) Формат JSON
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json')
+
+            const formData = new FormData(form);
+
+            const object = {}
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    showThanksModal(message.success);
+                    form.reset();
+                    statusMessage.remove();
+                } else {
+                    showThanksModal(message.failure);
+                }
+            })
+        })
+    }
+
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('hide');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000)
+    }
 });
