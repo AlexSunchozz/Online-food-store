a54c724f1674c8a8e843c4a64895caee28b4f251
Alexandr <arepkin96@mail.ru>
2023-05-07 20:46:03 +0300

    JSON-servel and async/await

--- js/scrips.js
+++ js/scrips.js
@@ -147,75 +147,86 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Card Class
 
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.classes = classes;
-            this.parent = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH();
+    // class MenuCard {
+    //     constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+    //         this.src = src;
+    //         this.alt = alt;
+    //         this.title = title;
+    //         this.descr = descr;
+    //         this.price = price;
+    //         this.classes = classes;
+    //         this.parent = document.querySelector(parentSelector);
+    //         this.transfer = 27;
+    //         this.changeToUAH();
+    //     }
+
+    //     changeToUAH() {
+    //         this.price = this.price * this.transfer;
+    //     }
+
+    //     render() {
+    //         const element = document.createElement('div');
+
+    //         if (this.classes.length === 0) {
+    //             this.element = 'menu__item'
+    //             element.classList.add(this.element)
+    //         } else {
+    //             this.classes.forEach(className => element.classList.add(className))
+    //         }
+
+    //         element.innerHTML = `
+    //             <img src=${this.src} alt=${this.alt}>
+    //             <h3 class="menu__item-subtitle">${this.title}</h3>
+    //             <div class="menu__item-descr">${this.descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+    //             </div>
+    //         `
+    //         this.parent.append(element)
+    //     }
+    // }
+
+    const getResource = async (url) => {
+        const res = await fetch(url)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
-        changeToUAH() {
-            this.price = this.price * this.transfer;
-        }
+        return await res.json();
+    }
 
-        render() {
+    // getResource('http://localhost:3000/menu')
+    // .then(data => {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //     });
+    // });
+
+    getResource('http://localhost:3000/menu')
+    .then(data => createCard(data))
+
+    function createCard(data) {
+        data.forEach(({img, altimg, title, descr, price}) => {
             const element = document.createElement('div');
-
-            if (this.classes.length === 0) {
-                this.element = 'menu__item'
-                element.classList.add(this.element)
-            } else {
-                this.classes.forEach(className => element.classList.add(className))
-            }
+            element.classList.add('menu__item');
 
             element.innerHTML = `
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
+                <img src=${img} alt=${altimg}>
+                <h3 class="menu__item-subtitle">${title}</h3>
+                <div class="menu__item-descr">${descr}</div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
                 </div>
-            `
-            this.parent.append(element)
-        }
+            `;
+
+            document.querySelector('.menu .container').append(element);
+        })
     }
-
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-    ).render();
-
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        20,
-        '.menu .container',
-        'menu__item'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        16,
-        '.menu .container',
-        'menu__item'
-    ).render();
 
 
     // Server Forms
@@ -229,7 +240,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
     
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     })
 
     // 1) Формат строчный
@@ -270,7 +281,19 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // 2) Формат JSON
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type" : "application/json"
+            },
+            body: data
+        })
+
+        return await res.json();
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -289,29 +312,28 @@ window.addEventListener('DOMContentLoaded', function() {
 
             const formData = new FormData(form);
 
-            const object = {}
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
+            // const object = {}
+            // formData.forEach(function(value, key) {
+            //     object[key] = value;
+            // });
+
+            // Аналог преобразование  FormData
+            const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
             // Fetch реализация вместо XMLHttpRequest
-
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    "Content-type" : "application/json"
-                },
-                body: JSON.stringify(object)
-            }).then(data => data.text()).then(data => {
+            
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
                 statusMessage.remove();
-            }).catch(() => {
+            })
+            .catch(() => {
                 showThanksModal(message.failure);
-            }).finally(() => {
+            })
+            .finally(() => {
                 form.reset();
             });
-
 
 
             // request.send(json);
@@ -353,4 +375,8 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000)
     }
+
+    this.fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(json => console.log(json))
 });
